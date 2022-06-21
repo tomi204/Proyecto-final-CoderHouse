95019b24e874315f54e4d8fb6813fa306831f838
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-06-21 02:17:27 -0300

    first commit

--- js/PROJECTS.js
+++ js/PROJECTS.js
@@ -5,10 +5,10 @@ const getSong = document.querySelector(".song");
 //  FUNCTION MUSIC PROJECTS
 getIcon.addEventListener("click", () => {
   if (getSong.paused) {
-    getIcon.src = "/Portafolio/img/pauseb.png";
+    getIcon.src = "../img/pauseb.png";
     getSong.play();
   } else {
-    getIcon.src = "/Portafolio/img/playicon.jpg";
+    getIcon.src = "../img/playicon.jpg";
     getSong.pause();
   }
 });
--- js/index.js
+++ js/index.js
@@ -5,10 +5,10 @@ const getSong = document.querySelector(".song");
 //  FUNCTION MUSIC HOME
 getIcon.addEventListener("click", () => {
   if (getSong.paused) {
-    getIcon.src = "img/pauseb.png";
+    getIcon.src = "../img/pauseb.png";
     getSong.play();
   } else {
-    getIcon.src = "img/playicon.jpg";
+    getIcon.src = "../img/playicon.jpg";
     getSong.pause();
   }
 });
